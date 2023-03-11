34c1fe19a6839c97292363e184b8ea63d1e82bf0
Mareczek1311 <103691090+Mareczek1311@users.noreply.github.com>
2023-03-12 00:21:35 +0100

    '12.03.2003'

--- src/App.js
+++ src/App.js
@@ -7,43 +7,145 @@ import { useState } from 'react';
 
 
 function App() {
+
+    function timeout(delay) {
+      return new Promise( res => setTimeout(res, delay) );
+  }
+    const [playersVal, setPlayersVal] = useState([[],[]]);
+    const [playersUI, setPlayersUI] = useState(["inGame", "inGame"]);
+    const [isDealerRound, setIsDealerRound] = useState(false);
+
     const cards = [
       4,4,4,4,4,4,4,4,7,4
     ]
+ 
+       
+    function getRandomCard(){
+      let card = Math.floor(Math.random() * 10);
+      while(cards[card] === 0)
+      {
+        card = Math.floor(Math.random() * 10);
+      }  
+      
+      cards[card]-=1;
     
-    const [dealerVal, setDealerVal] = useState(0);
-    const [playerVal, setPlayerVal] = useState(0);
+      card += 2; 
+      return card
+    }
+  function takeCard(index){
+    var card = getRandomCard()
+    
+    const newArray = playersVal.map((item, i) => {
+        return item;
+      
+    });
+
+    newArray[index].push(card)
+    setPlayersVal(newArray)
+    console.log(playersVal[index])
+  }
   
   function dealCard(){
-    let card = Math.floor(Math.random() * 10);
-    while(cards[card] === 0)
-    {
-      card = Math.floor(Math.random() * 10);
-    }  
-  
-      cards[card]-=1;
-  
-      card += 2;
-  
-      setDealerVal(dealerVal + card);
-      setPlayerVal(playerVal + card);
+    for(let i=0; i<playersVal.length; i++){
+      takeCard(i)
+    }
+
   }
   const [started, setStarted] = useState(true)
 
   function start(){
+    dealCard()
     dealCard()
     setStarted(false);
   
   }
   
   function hit(){
-  
+    // its one rn becouse im playing solo
+    takeCard(1)
+
+    let playerSum = sum(playersVal[1])
+    let plUI = playersUI
+
+    if(playerSum > 21){
+      plUI[1] = "lose"  
+    }
+
+    setPlayersUI(plUI)
   }
   
+  function sum(array){
+    var res = 0
+    for(let i=0; i<array.length; i++){
+      res += array[i]
+    }
+    return res
+  }
+
   function stand(){
-    
-  }
+    //Sould next player play but right now im gonna hard code that. I will make
+    //sure that game is playable 1v1 rn
+    setIsDealerRound(true)
+    //check if its the last player
+
+    // i assume that im the last player so its dealer round rn
+
+    let dealerSum = sum(playersVal[0])
+
+    if(dealerSum <= 16){
+      takeCard(0)
+    }
+    dealerSum = sum(playersVal[0])
+    let plUI = playersUI
   
+    let playerSum = sum(playersVal[1])
+    if(dealerSum > 21){
+      plUI[1] = "Win"
+    }
+    else if(playerSum > dealerSum){
+      plUI[1] = "Win"
+    }
+    else if(playerSum == dealerSum){
+      // Reset ??????
+    }
+    else{
+      plUI[1] = "lose"
+    }
+    setPlayersUI(plUI)
+  }
+
+  function move(index){
+    let cardSum = sum(playersVal[index])
+    let plUI = playersUI
+
+    if(cardSum == 21){
+      plUI[index] = "Win"
+    }
+    else if(cardSum > 21){
+      plUI[index] = "lose"
+
+    }
+    else{
+      plUI[index] = "inGame"
+      
+    }
+    setPlayersUI(plUI)
+  }
+
+  function turn(state){
+    if(state == "hit"){
+      hit()
+      move(1)
+
+    }
+
+    if(state == "stand"){
+      stand()
+    } 
+
+  }
+//await timeout(1000); //for 1 sec delay
+
   return (
     <div className="App">
       <div className="mainContainer">  
@@ -57,16 +159,48 @@ function App() {
               
             </> :
             <>
-              <h1>Dealer: {dealerVal}</h1>
-              <h1>You: {playerVal}</h1>
-              <div className='playButtons'>
-              <Button onClick={ () => stand() } className='playbtn' as="a" variant="primary">
-                Stand
-              </Button>
-              <Button onClick={ () => hit() } className='playbtn' as="a" variant="primary">
-                Hit
-              </Button>
-              </div>
+              <h1>Dealer: </h1>
+              <ul className="playerCardsList">
+              {playersVal[0].map((el, inx) => {
+                if(inx == 0)
+                {
+                  return(
+                <li key={inx}>{el}</li>)
+                }
+                else if(inx == 1 && isDealerRound == false){
+                  return( 
+                    <li key={inx}>?</li>)
+                }
+                else{
+                  return(
+                    <li key={inx}>{el}</li>)
+                }
+              })}
+              </ul>
+              
+                <h1>You:</h1>
+                      <ul className="playerCardsList">
+                        {playersVal[1].map((el, inx) => <li key={inx}>{el}</li>)}
+                      </ul>
+                  {playersUI[1] == "inGame" ? <>
+
+
+                      <div className='playButtons'>
+                        <Button onClick={ () => turn("stand") } className='playbtn' as="a" variant="primary">
+                          Stand
+                        </Button>
+                        <Button onClick={ () => turn("hit") } className='playbtn' as="a" variant="primary">
+                          Hit
+                        </Button>
+                     </div>
+
+                    </> 
+                    :
+                    playersUI[1] == "lose" ?<> 
+                    
+                    <p>Lose</p> </>: <p>Win</p>
+                   }
+
             </>
         }
         </header>
