87bc777e751337190f1cdd91cb34b1544a991bbb
Mareczek1311 <103691090+Mareczek1311@users.noreply.github.com>
2023-03-11 16:28:25 +0100

    '.'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+
+
+
 
 function App() {
+    const cards = [
+      4,4,4,4,4,4,4,4,7,4
+    ]
+    
+    const [dealerVal, setDealerVal] = useState(0);
+    const [playerVal, setPlayerVal] = useState(0);
+  
+  function dealCard(){
+    let card = Math.floor(Math.random() * 10);
+    while(cards[card] === 0)
+    {
+      card = Math.floor(Math.random() * 10);
+    }  
+  
+      cards[card]-=1;
+  
+      card += 2;
+  
+      setDealerVal(dealerVal + card);
+      setPlayerVal(playerVal + card);
+  }
+  const [started, setStarted] = useState(true)
+
+  function start(){
+    dealCard()
+    setStarted(false);
+  
+  }
+  
+  function hit(){
+  
+  }
+  
+  function stand(){
+    
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer">  
+        <header className="App-header">
+          {
+            started ? 
+            <>
+              <Button onClick={ () => start() } className='playbtn' as="a" variant="primary">
+                Start
+              </Button>
+              
+            </> :
+            <>
+              <h1>Dealer: {dealerVal}</h1>
+              <h1>You: {playerVal}</h1>
+              <div className='playButtons'>
+              <Button onClick={ () => stand() } className='playbtn' as="a" variant="primary">
+                Stand
+              </Button>
+              <Button onClick={ () => hit() } className='playbtn' as="a" variant="primary">
+                Hit
+              </Button>
+              </div>
+            </>
+        }
+        </header>
+      </div>
     </div>
   );
 }
